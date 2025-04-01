807d176278ecdadd0ab6682a5f94201edb0e8855
duda4418 <146849849+duda4418@users.noreply.github.com>
2025-04-01 18:39:55 +0300

    Initial commit
    Generated by react-native@0.78.2

---
+++ android/app/src/main/java/com/hsfinal_stage/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.hsfinal_stage
+
+import com.facebook.react.ReactActivity
+import com.facebook.react.ReactActivityDelegate
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
+import com.facebook.react.defaults.DefaultReactActivityDelegate
+
+class MainActivity : ReactActivity() {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  override fun getMainComponentName(): String = "HSFinal_Stage"
+
+  /**
+   * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
+   * which allows you to enable New Architecture with a single boolean flags [fabricEnabled]
+   */
+  override fun createReactActivityDelegate(): ReactActivityDelegate =
+      DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+}
